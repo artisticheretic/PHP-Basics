0a2a39a8fba65c56015989e5189d966b7c812276
Ishan Ghosh <ishan.ghosh@innoraft.com>
2023-04-04 18:54:16 +0530

    PHP Basics Practice
    Till Operators

---
+++ operators.php
@@ -0,0 +1,34 @@
+<?php
+
+    /*
+    1. Arithmetic Operators
+    2. Assignment Operators
+    3. Comparison Operators
+    4. Logical Operators
+    */
+
+    $a = 45;
+    $b = 7;
+?>
+
+<p>adgs</p>
+
+<?php
+    echo "When a and b are added, the result is " . ($a + $b) ;
+    echo "<br>";
+    echo "When a and b are added, the result is " . ($a - $b) ;
+    echo "<br>";
+    echo "When a and b are added, the result is " . ($a * $b) ;
+    echo "<br>";
+    echo "When a and b are added, the result is " . ($a / $b) ;
+    echo "<br>";
+    echo "When a and b are added, the result is " . ($a ** $b) ;
+?>
+
+<br>
+
+<?php
+
+
+
+?>
