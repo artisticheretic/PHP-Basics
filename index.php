51443e42122b7afa89774a1c5d18efe284627bba
Ishan Ghosh <ishan.ghosh@innoraft.com>
2023-04-04 17:44:08 +0530

    PHP Basics Practice 1

--- index.php
+++ index.php
@@ -1,130 +1,16 @@
-<?php
-
-    /* echo ("Hello World");
-    echo "<br>";
-    echo 9+53;
-    echo "<br>";
-    echo "This ", "Is ", "Different!"; */
-
-    // $var1 = "Learning PHP";
-    // $var2 = "from w3schools.com";
-    // $x = 5;
-    // $y = 7;
-
-    // echo "<h2>" .$var1. "</h2>";
-    // echo "Studying PHP " .$var2.  "<br>";
-    // echo $x + $y ;
-
-    // print "<h3> This is an example of a sum" .$x+$y. "</h3>"; 
-
-    // $x = [2, "This is string"];
-    // var_dump($x);
-
-    // echo "<br>";
-
-    // $a = true;
-    // $b = false;
-    // var_dump($b);
-
-    /*
-    class Car {
-        public $color;
-        public $model;
-        public function __construct($color, $model) {
-          $this->color = $color;
-          $this->model = $model;
-        }
-        public function message() {
-          return "My car is a " . $this->color . " " . $this->model . "!";
-        }
-      }
-      
-      $myCar = new Car("black", "Volvo");
-      echo $myCar -> message();
-      echo "<br>";
-      $myCar = new Car("red", "Toyota");
-      echo $myCar -> message();
-    */
-
-    // echo strlen ("Hello World");
-    // echo "<br>";
-    // echo str_word_count("There are a total of 7 words here");
-    // echo "<br>";
-    // echo strrev ("This is the reverse of this sentence!");
-    // echo "<br>";
-    // echo strpos ("Hello World!", "H"); // Output - 0, Because Index(0) = H
-    // echo "<br>";
-    // echo str_replace ("There", "Here", "There she is");
-
-    // $x = 5985;
-    // var_dump(is_float($x));
-
-    // echo "<br>";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo "This is"; echo " Title" ?></title>
+</head>
+<body>
     
-    // $y = "This is a string, not a integer";
-    // var_dump(is_string($y));
+    <?php
+        echo "Hello World";
+    ?>
 
-    // echo "<br>";
-
-    // $x = 1.9e41;
-    // var_dump($x);
-
-    // echo "<br>";
-
-    // $x = 1.9e410;
-    // var_dump($x);
-
-
-    // Cast float to int
-    // $x = 23465.768;
-    // $int_cast = (int)$x;
-    // echo $int_cast;
-
-    // echo "<br>";
-
-    // Cast string to float
-    // $x = "23465.768";
-    // $int_cast = (float)$x;
-    // echo $int_cast;
-
-    // Constants are global 
-
-    // define("GREETING", "Welcome to W3Schools.com!");
-    // echo GREETING;
-
-    // echo "<br>";
-
-    // define("cars", ["Alfa Romeo", "BMW", "Toyota"]);
-    //   echo cars[1]; // Outputs the value at its respective index
-
-    // $x = 5;
-    // $y = 10;
-    // echo (++$x);
-    // echo "<br>";
-    // echo ($y++);
-
-    // echo "<br>";
-
-    // $a = 20;
-    // $b = 30;
-    // echo (--$a);
-    // echo "<br>";
-    // echo ($b--);
-
-    // $favcolor = "blue";
-    
-    // switch ($favcolor) {
-    //     case "red":
-    //         echo "Your favorite color is red!";
-    //         break;
-    //     case "blue":
-    //         echo "Your favorite color is blue!";
-    //         break;
-    //     case "green":
-    //         echo "Your favorite color is green!";
-    //         break;
-    //     default:
-    //     echo "Your favorite color is neither red, blue, nor green!";
-    // }
-    
-?>
+</body>
+</html>
